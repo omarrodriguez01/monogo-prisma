adabc76fceb08735a77cd0def5cc15a75a1986dd
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-06-06 11:11:15 -0600

    chore: Update schema and controller for Students module

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -6,6 +6,6 @@ import { PrismaModule } from '../prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   controllers: [StudentsController],
-  providers: [StudentsService]
+  providers: [StudentsService],
 })
 export class StudentsModule {}
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,24 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Prisma, Student } from '@prisma/client';
+import { Student } from '@prisma/client';
 
 @Injectable()
 export class StudentsService {
   constructor(private prisma: PrismaService) {}
 
   async findAll(): Promise<Student[]> {
-    return [];
+    return this.prisma.student.findMany();
   }
 
   async findById(id: string): Promise<Student | null> {
-    return null;
+    return this.prisma.student.findUnique({
+      where: { id },
+    });
   }
 
   async findAllWithTeamName(): Promise<Student[]> {
-    return [];
+    return this.prisma.student.findMany({
+      where: {
+        team: {
+          name: {
+            not: null,
+          },
+        },
+      },
+    });
   }
 
-  async create(data: { name: string; generation: number; teamId: string }): Promise<Student | null> {
-    return null;
+  async create(data: {
+    name: string;
+    generation: number;
+    teamId: string;
+  }): Promise<Student | null> {
+    return this.prisma.student.create({
+      data: {
+        name: data.name,
+        generation: data.generation,
+        team: {
+          connect: {
+            id: data.teamId,
+          },
+        },
+      },
+    });
   }
 }
