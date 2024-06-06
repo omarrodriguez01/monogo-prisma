0bfd8f2d0fcbab18c0f78f41c554a055c7696245
RoJosGaRis <roberto.jgr03@gmail.com>
2024-06-06 10:35:43 -0600

    first commit

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -2,28 +2,33 @@ import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { Student } from '@prisma/client';
 
-
-
 @Controller('students')
 export class StudentsController {
-    constructor(private readonly studentsService: StudentsService) {}
-    @Get()
-    async findAll(): Promise<Student[]> {
-      return this.studentsService.findAll();
-    }
+  constructor(private readonly studentsService: StudentsService) {}
+  @Get()
+  async findAll(): Promise<Student[]> {
+    return this.studentsService.findAll();
+  }
 
-    @Get('/withTeamName')
-    async findAllWithTeamName(): Promise<Student[]> {
-      return this.studentsService.findAllWithTeamName();
-    }
+  @Get('/withTeamName')
+  async findAllWithTeamName(): Promise<Student[]> {
+    return this.studentsService.findAllWithTeamName();
+  }
 
-    @Get(':id')
-    findById(@Param('id') id: string): Promise<Student | null> {
-      return this.studentsService.findById(id);
-    }
-  
-    @Post()
-    create(@Body() createStudentDto: { name: string; generation: number; teamId: string }): Promise<Student> {
-      return this.studentsService.create(createStudentDto);
-    }
+  @Get(':id')
+  findById(@Param('id') id: string): Promise<Student | null> {
+    return this.studentsService.findById(id);
+  }
+
+  @Post()
+  create(
+    @Body()
+    createStudentDto: {
+      name: string;
+      generation: number;
+      teamId: string;
+    },
+  ): Promise<Student> {
+    return this.studentsService.create(createStudentDto);
+  }
 }
